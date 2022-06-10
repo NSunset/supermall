e63e83f5538bbeb6c43075557e31b474f0a82a26
nw <524873829@qq.com>
2022-06-10 09:31:07 +0800

    划分目录结构和基础配置

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <div id="App"></div>
 </template>
 
 <script>
@@ -9,4 +9,6 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import url("assets/css/base.css");
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      //配置路径别名,@默认注册的别名路径是/src
+      alias: {
+        assets: "@/assets",
+        common: "@/common",
+        components: "@/components",
+        network: "@/network",
+        router: "@/router",
+        views: "@/views",
+      },
+    },
+  },
+});
